664d31808e7f14be08277bcb8a323158c8a8fbfe
Felipe34515 <f.rueda4@uniandes.edu.co>
2023-11-30 09:09:46 -0500

    cositas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { PerformerService } from './performer/performer.service';
+import { PerformerModule } from './performer/performer.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule, TrackModule, PerformerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PerformerService],
 })
 export class AppModule {}
