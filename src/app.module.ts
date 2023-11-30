1be6b8191a742bb372eb1cb08ed309d8059e1724
Felipe34515 <f.rueda4@uniandes.edu.co>
2023-11-30 09:35:44 -0500

    Primer commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FotoModule } from './foto/foto.module';
+import { RedsocialModule } from './redsocial/redsocial.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [],
+  imports: [FotoModule, RedsocialModule, UsuarioModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
